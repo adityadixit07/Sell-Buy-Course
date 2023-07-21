3dfa2ac6cb587aa6b8f620d75bb8867af968f8ee
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2023-07-21 09:00:06 +0530

    done with payment verfication

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -2,38 +2,106 @@ import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { instance } from "../server.js";
+import crypto from "crypto";
+import { Payment } from "../models/Payment.js";
 
 export const buySubscription = catchAsyncError(async (req, res, next) => {
   const user = await User.findById(req.user._id);
-  if (user.role=== "admin") {
+  if (user.role === "admin") {
     return next(new ErrorHandler("Admin can't buy the subscription", 404));
   }
-  const plan_id = process.env.PLAN_ID;
-  const subscription =await instance.subscriptions.create({
-    plan_id,
+  const plan_id = process.env.PLAN_ID || "plan_7wAosPWtrkhqZw";
+  const subscription = await instance.subscriptions.create({
+    plan_id:plan_id,
     customer_notify: 1,
     total_count: 12,
   });
+  // console.log(subscription);
 
-  user.subscription.id=subscription.id;
-  user.subscription.status=subscription.status;
+  user.subscription.id = subscription.id;
+  user.subscription.status = subscription.status;
 
   await user.save();
 
   res.status(201).json({
-    success:true,
-    // subscription,
-    subscriptionId:subscription.id
-  })
+    success: true,
+    // subscription: subscription,
+    subscriptionId: subscription.id,
+  });
 });
 
-
 // payment verification
-export const paymentVerification=catchAsyncError(async(req,res,next)=>{
-  const {razorpay_signature,razorpay_payment_id,razorpay_subscription_id}=req.body;
+export const paymentVerification = catchAsyncError(async (req, res, next) => {
+  const { razorpay_signature, razorpay_payment_id, razorpay_subscription_id } =
+    req.body;
 
-  const user=await User.findById(req.user._id);
-  if(user.role==='admin'){
-    return next(new ErrorHandler("Admin can't buy subscription",404));
+  const user = await User.findById(req.user._id);
+
+  const subscription_id = user.subscription.id;
+
+  const generated_signature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(razorpay_payment_id + "|" + subscription_id, "utf-8")
+    .digest("hex");
+
+  const isAuthentic = generated_signature === razorpay_signature;
+  if (!isAuthentic) {
+    return res.redirect(`${process.env.FRONTEND_URL}/paymentfailed`);
   }
-})
+  // databse comes here
+  await Payment.create({
+    razorpay_signature,
+    razorpay_payment_id,
+    razorpay_subscription_id,
+  });
+
+  user.subscription.status = "active";
+  await user.save();
+
+  res.redirect(
+    `${process.env.FRONTEND_URL}/paymentsuccess?refrence=${razorpay_payment_id}`
+  );
+});
+
+// get razor pay key
+export const getRazorPayKey = catchAsyncError(async (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    key: process.env.RAZORPAY_API_KEY,
+  });
+});
+
+// Cancel subscription
+export const cancelSubscription = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user._id);
+
+  const subscriptionId = user.subscription.id;
+
+  let refund = false;
+
+  await instance.subscriptions.cancel(subscriptionId);
+  const payment = await Payment.findOne({
+    razorpay_subscription_id: subscriptionId,
+  });
+
+  const gap = Date.now() - payment.createdAt;
+
+  const refundTime = process.env.REFUND_DAYS * 24 * 60 * 60 * 1000;
+
+  if (refundTime > gap) {
+    // await instance.payments.refund(payment.razorpay_payment_id);
+    refund = true;
+  }
+
+  await payment.deleteOne();
+  user.subscription.id = undefined;
+  user.subscription.status = undefined;
+  await user.save();
+
+  res.status(200).json({
+    success: true,
+    message: refund
+      ? "Subscription cancelled. You will receive your refund with 7 days."
+      : "Subscription Cancelled. No refund was initiated as subscription was cancelled after 7 days.",
+  });
+});
