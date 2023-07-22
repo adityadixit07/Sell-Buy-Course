b01e92d3509a87d6112653a9657948ba187a69de
Aditya Kumar Dixit <96368151+adityadixit07@users.noreply.github.com>
2023-07-22 16:16:35 +0530

    all

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -175,14 +175,15 @@ export const deleteLectures = catchAsyncError(async (req, res, next) => {
   });
 });
 
-// Course.watch().on("change",async()=>{
-//   const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
-//   const courses=await Course.find({});
-//   let totalViews=0;
-//   for(let i=0;i<courses.length;i++){
-//     totalViews+=courses[i].views;
-//   }
-//   stats[0].views=totalViews;
-//   stats[0].createdAt=new Date(Date.now());
-//   await stats[0].save();
-// })
+
+Course.watch().on("change",async()=>{
+  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+  const courses=await Course.find({});
+  let totalViews=0;
+  for(let i=0;i<courses.length;i++){
+    totalViews+=courses[i].views;
+  }
+  stats[0].views=totalViews;
+  stats[0].createdAt=new Date(Date.now());
+  await stats[0].save();
+})
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -333,11 +333,11 @@ export const deleteMyProfile = catchAsyncError(async (req, res, next) => {
     });
 });
 
-// User.watch().on("change", async () => {
-//   const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
-//   const subscription = await User.find({ "subscription.status": "active" });
-//   stats[0].users = await User.countDocuments();
-//   stats[0].subscription = subscription.length;
-//   stats[0].createdAt = new Date(Date.now());
-//   await stats[0].save();
-// });
+User.watch().on("change", async () => {
+  const stats = await Stats.find({}).sort({ createdAt: "desc" }).limit(1);
+  const subscription = await User.find({ "subscription.status": "active" });
+  stats[0].users = await User.countDocuments();
+  stats[0].subscription = subscription.length;
+  stats[0].createdAt = new Date(Date.now());
+  await stats[0].save();
+});
